16435dd6b97297d34f06191b17a36081cbb91fc3
Matt Ebb <matt@mattebb.com>
2012-12-24 10:52:57 +1100

    Parameter, timeline tweaks

--- main.py
+++ main.py
@@ -29,13 +29,14 @@ pyglet.options['debug_gl'] = False
 #pyglet.options['debug_graphics_batch'] = True
 #pyglet.options['debug_gl_trace'] = True
 #pyglet.options['debug_gl_trace_args'] = True
-from pyglet.gl import *
+#from pyglet.gl import *
 
 def init():
     
     from euclid import Vector3, Point3, Matrix4
 
-    import ui2d
+    #import ui2d
+    from ui2d import Ui, UiLayout, UiControls
 
     from parameter import Parameter, Color3
     from camera import Camera
@@ -43,15 +44,15 @@ def init():
     import os
     import sys
 
-    window = pyglet.window.Window(900, 400, resizable=True)
+    window = pyglet.window.Window(1024, 500, resizable=True)
 
     scene = Scene()
     scene.camera = Camera(window)
     pyglet.clock.schedule(scene.update)
     
-    glClearColor(0.32, 0.32, 0.32, 1)
+    pyglet.gl.glClearColor(0.32, 0.32, 0.32, 1)
  
-    ui = ui2d.Ui(window, layoutw=0.2)
+    ui = Ui(window, layoutw=0.25)
     ui.control_types['numeric'] += [Point3, Vector3]
     ui.control_types['color'] +=  [Color3]
 
@@ -80,19 +81,16 @@ def init():
                     seeking = False
                     break
 
-    
-
-    scene.sframe = minframe
-    scene.eframe = maxframe
-    print('min frame', scene.sframe, 'max frame', scene.eframe)
-    scene.frame.setval( minframe )
+    scene.sframe.value = minframe
+    scene.eframe.value = maxframe
+    scene.frame.value =  minframe
 
     scene.pointclouds = []
     for filename in sys.argv[1:]:
         pointcloud = Ptc(scene, filename)
         scene.pointclouds.append( pointcloud )
         layout = ui.layout.addLayout(bg=True)
-        layout.addParameter(ui, pointcloud.visible, title=filename)
+        layout.addParameter(ui, pointcloud.visible, title=filename[-28:])
         
         layout.addParameter(ui, pointcloud.decimate)
         layout.addLabel(ui, param=pointcloud.num_particles)
@@ -100,9 +98,10 @@ def init():
         layout.addLabel(ui, title=' ') # Separator
 
         layout.addParameter(ui, pointcloud.attributes)
+        layout.addParameter(ui, pointcloud.show_statistics)
         layout.addLabel(ui, param=pointcloud.attr_stats)
-        #layout.addParameter(ui, pointcloud.translate)
-
+        layout.addParameter(ui, pointcloud.histogram)
+        
         ui.layout.addLabel(ui, title=' ') # Separator
     
     ptch = ptc.PtcHandler(scene, window)
@@ -116,15 +115,25 @@ def init():
     layout.addParameter(ui, Ptc.exposure)
     layout.addParameter(ui, Ptc.hueoffset)
     
-    scene.camera.fieldofview = Parameter(object=scene.camera, attr="fov", subtype=ui2d.UiControls.ANGLE, update=scene.camera.update_projection, vmin=0.087, vmax=2.618)
-    layout.addParameter(ui, scene.camera.fieldofview)
+    #scene.camera.fieldofview = Parameter(object=scene.camera, attr="fov", subtype=UiControls.ANGLE, update=scene.camera.update_projection, vmin=0.087, vmax=2.618)
+    #layout.addParameter(ui, scene.camera.fieldofview)
     
     ui.layout.addLabel(ui, title=' ') # Separator
 
     layout = ui.layout.addLayout(bg=True)
+    # horz = layout.addLayout(bg=False, style=UiLayout.HORIZONTAL)
+    # def sframe():
+    #     scene.frame.value = scene.sframe.value
+    # def eframe():
+    #     scene.frame.value = scene.eframe.value
+    # horz.addControl(ui, func=sframe, title='<')
+    # horz.addParameter(ui, scene.playback)
+    # horz.addControl(ui, func=eframe, title='>')
+
     layout.addParameter(ui, scene.playback)
+    layout.addParameter(ui, scene.sframe)
+    layout.addParameter(ui, scene.eframe)
     layout.addParameter(ui, scene.frame)
-    #ui.layout.print_r()
 
     
 
@@ -150,5 +159,5 @@ if __name__ == "__main__":
     # cProfile.run('pyglet.app.run()', '/tmp/pyprof')
     # import pstats
     # stats = pstats.Stats('/tmp/pyprof')
-    # stats.sort_stats('cumulative')
+    # stats.sort_stats('time')
     # stats.print_stats(50)
